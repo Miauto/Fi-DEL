7413a783c3aceef0cebd6b1f420cbc427251b866
Miauto <matiou.k@gmail.com>
2016-07-14 19:51:23 +0200

    Update Fi-DEL.ino

--- Arduino/Fi-DEL/Fi-DEL.ino
+++ Arduino/Fi-DEL/Fi-DEL.ino
@@ -2,30 +2,35 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <Adafruit_NeoPixel.h>
+
 
 #include "Page_Script.js.h"
-#include "html_pages.h"
+#include "Pages_html.h"
 #include "LedAnim.h"
+//#include "Animation.h"
+#include "Page_rangeslider.css.h"
 
-#define PIN   2
-#define NbLED 4
 #define colorSaturation 255
-String Choix = "Rainbow"; // demarre sur l'effet Rainbow
+String Choix = "Fire";
 String Couleur = "";
 String Vitesse = "";
-byte Rgb = 0;
-byte rGb = 0;
-byte rgB = 0;
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NbLED, PIN, NEO_GRB + NEO_KHZ800);
+byte rainbowIndex;
+byte theaterChaseRainbowIndex;
+byte RainbowCycleIndex;
 
-const char *ssid[] = {"SSID1", "SSID2", "SSID3", "SSID4"};
-const char *password[] = {"PASS1", "PASS2", "PASS3", "PASS4"};
+const int nbssid = 2;
 
+const char *ssid[] = {
+//  "ssid1",
+//  "ssid2",
+};
+const char *password[] = {
+//  "mdp1",
+//  "mdp2",
+};
 
 int ErrComm = 0;
-
 
 MDNSResponder mdns;
 ESP8266WebServer server ( 80 );
@@ -33,20 +38,20 @@ ESP8266WebServer server ( 80 );
 
 void handleRoot() {
   server.send ( 200, "text/html", main_page );
-//  for ( uint8_t i = 0; i < server.args(); i++ ) {
-//    if (server.argName (i) == "couleur" ) {
-//      Couleur = server.arg(i);
-//    }
-//    if (server.argName (i) == "vitesse" ) {
-//      Vitesse = server.arg(i);
-//    }
-//    out += i + ": ";
-//    out += server.argName (i) + "= ";
-//    out += server.arg(i) + "\n";
-//  }
-  Couleur = server.arg(0);
-  Vitesse = server.arg(1);
-//  server.send ( 200, "text/html", out );
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    if (server.argName (i) == "couleur" ) {
+      Couleur = server.arg(i);
+    }
+    if (server.argName (i) == "vitesse" ) {
+      Vitesse = server.arg(i);
+    }
+    //    out += i + ": ";
+    //    out += server.argName (i) + "= ";
+    //    out += server.arg(i) + "\n";
+  }
+  //  Couleur = server.arg(0);
+  //  Vitesse = server.arg(1);
+  //  server.send ( 200, "text/html", out );
 }
 void handleNotFound() {
   String message = "File Not Found\n\n";
@@ -65,177 +70,45 @@ void handleNotFound() {
   server.send ( 404, "text/plain", message );
 }
 
-///////////////////////
-// Les fonctions LED //
-///////////////////////
-
-void showStrip() {
-#ifdef ADAFRUIT_NEOPIXEL_H
-  // NeoPixel
-  strip.show();
-#endif
-#ifndef ADAFRUIT_NEOPIXEL_H
-  // FastLED
-  FastLED.show();
-#endif
-}
-
-void setPixel(int Pixel, byte red, byte green, byte blue) {
-#ifdef ADAFRUIT_NEOPIXEL_H
-  // NeoPixel
-  strip.setPixelColor(Pixel, strip.Color(red, green, blue));
-#endif
-#ifndef ADAFRUIT_NEOPIXEL_H
-  // FastLED
-  leds[Pixel].r = red;
-  leds[Pixel].g = green;
-  leds[Pixel].b = blue;
-#endif
-}
-
-void setAll(byte red, byte green, byte blue) {
-  for (int i = 0; i < NbLED; i++ ) {
-    setPixel(i, red, green, blue);
-  }
-  strip.show();
-}
-
-void HexatoDec(String Hexa) {
-  // Get rid of '#' and convert it to integer
-  int number = (int) strtol( &Hexa[1], NULL, 16);
-
-  // Split them up into r, g, b values
-  Rgb = number >> 16;
-  rGb = number >> 8 & 0xFF;
-  rgB = number & 0xFF;
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if (WheelPos < 85) {
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-    WheelPos -= 170;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
-
-// Rainbow
-void rainbow(uint8_t wait, unsigned char* j) {
-  int i;
-
-  *j = *j + 1;
-  for (i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, Wheel((i + *j) & 255));
-  }
-  strip.show();
-  delay(wait);
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait,  unsigned char* j) {
-  *j = *j + 1;   // cycle all 256 colors in the wheel
-  for (int q = 0; q < 3; q++) {
-    for (int i = 0; i < strip.numPixels(); i = i + 3) {
-      strip.setPixelColor(i + q, Wheel( (i + *j) % 255)); //turn every third pixel on
-    }
-    strip.show();
-
-    delay(wait);
-
-    for (int i = 0; i < strip.numPixels(); i = i + 3) {
-      strip.setPixelColor(i + q, 0);      //turn every third pixel off
-    }
-  }
-
-}
-
-// effet neige
-void SnowSparkle(byte red, byte green, byte blue, int SparkleDelay, int SpeedDelay) {
-  setAll(red, green, blue);
-
-  int Pixel = random(NbLED);
-  setPixel(Pixel, 0xff, 0xff, 0xff);
-  showStrip();
-  delay(SparkleDelay);
-  setPixel(Pixel, red, green, blue);
-  showStrip();
-  delay(SpeedDelay);
-}
-
-// Strobe
-void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause) {
-  for (int j = 0; j < StrobeCount; j++) {
-    setAll(red, green, blue);
-    showStrip();
-    delay(FlashDelay);
-    setAll(0, 0, 0);
-    showStrip();
-    delay(FlashDelay);
-  }
-
-  delay(EndPause);
-}
-
-/*
-void LeftToRight(byte red, byte green, byte blue, int SpeedDelay, int ReturnDelay) {
-  for (int i = 0; i < NbLED; i++) {
-    setAll(red / 100, green / 100, blue / 100);
-
-    setPixel(i, red, green, blue);
-
-    showStrip();
-    delay(SpeedDelay);
-  }
-  delay(ReturnDelay);
-}
-*/
-
-void initLED()
-{
-  setAll(127, 0, 0);
-  delay(500);
-  setAll(0, 127, 0);
-  delay(500);
-  setAll(0, 0, 127);
-  delay(500);
-  setAll(127, 127, 127);
-  delay(1000);
-  setAll(0, 0, 0);
-}
-
-
-
+ 
 void setup ( void ) {
   Serial.begin ( 115200 );
-
+  
   strip.begin();
   strip.show();
 
-  int i = 1;
-    WiFi.begin ( ssid[i], password[i] );
-    Serial.println ( "" );
-    // Wait for connection
-    while ( WiFi.status() != WL_CONNECTED ) {
-        delay ( 500 );
-        Serial.print ( "." );
-        ErrComm = ErrComm + 1;
-        if (ErrComm > 10) {
-          i++;
-          ErrComm = 0;
-          WiFi.begin ( ssid[i], password[i] );
-        }
+  int SSID_N = 0;
+  Serial.println ( "" );
+  Serial.println ( "Connection au SSID : " );
+  Serial.print ( ssid[SSID_N] );
+  WiFi.begin ( ssid[SSID_N], password[SSID_N] );
+  Serial.println ( "" );
+  // Wait for connection
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+    Serial.print ( ErrComm );
+    ErrComm = ErrComm + 1;
+    if (ErrComm > 15) {
+      SSID_N++;
+      if (SSID_N > nbssid) {
+        SSID_N = 0;
+      }
+      ErrComm = 0;
+      Serial.println ( "" );
+      Serial.println ( "SSID non present" );
+      Serial.println ( "Connection au SSID : " );
+      Serial.print ( ssid[SSID_N] );
+      Serial.println ( "" );
+      WiFi.begin ( ssid[SSID_N], password[SSID_N] );
     }
-    Serial.println ( "" );
-    Serial.print ( "Connected to " );
-    Serial.println ( ssid[i] );
-    Serial.print ( "IP address: " );
-    Serial.println ( WiFi.localIP() );
-    
+  }
+  Serial.println ( "" );
+  Serial.print ( "Connecte a " );
+  Serial.println ( ssid[SSID_N] );
+  Serial.print ( "IP Adresse: " );
+  Serial.println ( WiFi.localIP() );
+
   if ( mdns.begin ( "esp8266", WiFi.localIP() ) ) {
     Serial.println ( "MDNS responder started" );
   }
@@ -246,6 +119,14 @@ void setup ( void ) {
     Choix = "Rainbow";
     handleRoot();
   } );
+  server.on ( "/rain", []()  {
+    Choix = "Rainbow";
+    handleRoot();
+  } );
+    server.on ( "/RainbowCycle", []()  {
+    Choix = "RainbowCycle";
+    handleRoot();
+  } );
   server.on ( "/theatre", []()  {
     Choix = "theaterChaseRainbow";
     handleRoot();
@@ -254,39 +135,71 @@ void setup ( void ) {
     Choix = "neige";
     handleRoot();
   } );
+  server.on ( "/TwinkleRandom", []()    {
+    Choix = "TwinkleRandom";
+    handleRoot();
+  } );
   server.on ( "/Gyro", []()    {
     Choix = "Gyro";
+    handleRoot();
+  } );
+  server.on ( "/Fire", []()    {
+    Choix = "Fire";
     handleRoot();
   } );
   server.on ( "/color", []()    {
     Choix = "color";
     handleRoot();
   } );
-  server.on ( "/SendAll.js", []() { Serial.println("SendAll.js"); server.send ( 200, "text/plain", PAGE_SendAll_js );  } );
+    server.on ( "/config", []() {
+    server.send ( 200, "text/plain", config_page );
+  } );
+  server.on ( "/SendAll.js", []() {
+    server.send ( 200, "text/plain", PAGE_SendAll_js );
+  } );
+  server.on ( "/rangeslider.js", []() {
+    server.send ( 200, "text/plain", PAGE_rangeslider_js );
+  } );
+  server.on ( "/rangeslider.min.js", []() {
+    server.send ( 200, "text/plain", PAGE_rangeslider_min_js );
+  } );
+    server.on ( "/Picker.js", []() {
+    server.send ( 200, "text/plain", PAGE_Picker_js );
+  } );
+  server.on ( "/rangeslider.css", []() {
+    server.send ( 200, "text/css", PAGE_rangeslider_css );
+  } );
   server.onNotFound ( handleNotFound );
   server.begin();
   Serial.println ( "HTTP server started" );
   initLED();
+  
+  digitalWrite(2, LOW); //extinction de la LED Bleu du ESP
   Serial.println ( "Init Finish" );
 }
 
 
 void loop ( void ) {
-  unsigned char rainbowIndex;
-  unsigned char theaterChaseRainbowIndex;
-
+  
   mdns.update();
   server.handleClient();
   HexatoDec(Couleur);
 
   if (Choix == "neige") {
-    SnowSparkle(0x10, 0x10, 0x10, 20, random(100, 1000));
+    SnowSparkle(0x10, 0x10, 0x10, 100, random(200, 1000));
   } else if (Choix == "theaterChaseRainbow") {
     theaterChaseRainbow(50, &theaterChaseRainbowIndex);
   } else if (Choix == "Rainbow") {
     rainbow(500, &rainbowIndex);
+  } else if (Choix == "RainbowCycle") {
+    RainbowCycle(&RainbowCycleIndex);
   } else if (Choix == "Gyro") {
     LeftToRight(255, 0, 0, 75, 0);
+ } else if (Choix == "TwinkleRandom") {
+    TwinkleRandom(500, false);
+ } else if (Choix == "Fire") {
+//    Fire();          // V1
+    Fire(1,1000,100); // V2
   } else if (Choix == "color") {
 
     if ( Vitesse.toInt() > 0 ) {
@@ -296,11 +209,11 @@ void loop ( void ) {
     }
   }
 
-  if (Choix == "") {} else {
-    Serial.println (Choix);
+  if (Choix != "") {
+//    Serial.println (Choix);
   }
-  if (Couleur == "") {} else {
-    Serial.println (Couleur);
+  if (Couleur != "") {
+//    Serial.println (Couleur);
   }
 
 }
